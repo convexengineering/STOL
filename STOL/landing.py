5777f022d785ad18b2da1c87ea180b4e5aee4c19
Chris Courtin <chris_courtin@dhcp-18-189-114-109.dyn.mit.edu>
2017-10-13 16:36:55 -0400

    Added landing distance calculation landing.py; revived stol_aircraft as a library of test aircraft cases

---
+++ STOL/landing.py
@@ -0,0 +1,73 @@
+" Landing distance model "
+import os
+import pandas as pd
+from gpkit import Variable, Model, SignomialsEnabled, units
+from gpfit.fit_constraintset import FitCS
+from gpkit.constraints.tight import Tight as TCS
+from flightstate import FlightState
+from gpkit.tools.tools import te_exp_minus1 as em1 
+from stol_aircraft import testAircraft, HelioCourier
+
+class Landing(Model):
+    """
+    Landing model
+    http://www.dept.aoe.vt.edu/~lutze/AOE3104/takeoff&landing.pdf
+    
+    Landing_Margin is required additional distance for takeoff and landing operations (nominally 20%)
+    """
+    def setup(self, aircraft, sp=False):
+
+        fs      = FlightState()
+
+        A       = Variable("A", "m/s**2", "log fit equation helper 1")
+        B       = Variable("B", "1/m", "log fit equation helper 2")
+
+        g       = Variable("g", 9.81, "m/s**2", "gravitational constant")
+        mu      = Variable("\\mu", 0.5, "-", "Braking coefficient of friction")
+        T_rev   = Variable("T", "lbf", "Reverse thrust")
+        cda     = Variable("CDA", 0.024, "-", "parasite drag coefficient")
+
+        CLg     = Variable("C_{L_g}", "-", "ground lift coefficient")
+        CDg     = Variable("C_{D_g}", "-", "grag ground coefficient")
+        Kg      = Variable("K_g", 0.04, "-", "ground-effect induced drag parameter")
+        #CLmax   = Variable("C_{L_{max}}", 2.5, "-", "max lift coefficient")
+        Vstall  = Variable("V_{stall}", "m/s", "stall velocity")
+        Sgr     = Variable("S_{gr}", "ft", "landing ground roll")
+        Slnd    = Variable("S_{land}", "ft", "landing distance")
+        etaprop = Variable("\\eta_{prop}", 0.01, "-", "propellor efficiency")
+        lnd_mrg = Variable("Landing Margin", 1.2, "-", "Landing safety margin")
+
+        CLmax   = aircraft["C_{L_{max, land}}"]
+        constraints = [
+            TCS([A/g >=  T_rev/aircraft["W"] + mu]),
+            T_rev == aircraft["P_{shaft-max}"]*etaprop/fs["V"],
+            TCS([CDg >= cda + Kg*CLg**2]),
+            CLg == CLmax/1.2**.5,
+            Vstall == (2.*aircraft["W"]/fs["\\rho"]/aircraft["S"]/CLmax)**0.5,
+            fs["V"] == 1.2*Vstall,
+            TCS([em1(Sgr*2*B,3) <= B*fs["V"]**2./A]), #May be SP...
+            Slnd == lnd_mrg * Sgr,
+            ]
+        if sp:
+            with SignomialsEnabled():
+                constraints.extend([
+                    (B*aircraft["W"]/g + 0.5*fs["\\rho"]*aircraft["S"]*mu
+                     * CLg >= 0.5*fs["\\rho"]*aircraft["S"]*CDg)])
+        else:
+            constraints.extend([
+                B >= g/aircraft["W"]*0.5*fs["\\rho"]*aircraft["S"]*CDg])
+
+        return constraints, fs
+
+if __name__ == "__main__":
+    ac  = testAircraft()
+    M   = Landing(ac)
+    M.cost = 1/M["S_{land}"]
+    sol = M.solve("mosek")
+    print sol.table()
+
+    hc = HelioCourier()
+    M  = Landing(hc)
+    M.cost = 1/M["S_{land}"]
+    sol    = M.solve("mosek")
+    print sol.table()  
---
+++ STOL/stol_aircraft.py
@@ -0,0 +1,68 @@
+" short take off and landing aircraft model "
+from numpy import pi
+from gpkit import Variable, Model
+from takeoff import TakeOff
+from flightstate import FlightState
+
+# pylint: disable=too-many-locals, invalid-name, unused-variable
+class testAircraft(Model):
+    def setup(self):
+        W             = Variable("W", 2540, "lbf", "aircraft weight")
+        S             = Variable("S", 55.17,"ft**2", "wing planform area")
+        CL_max        = Variable("C_{L_{max, land}}", 2.5,"-", "Landing CL_max")
+        Pshaftmax     = Variable("P_{shaft-max}", 200., "hp", "Maximum shaft power")
+        return []
+
+class HelioCourier(Model):
+    def setup(self):
+        W           = Variable("W", 3400, "lbf", "aircraft weight")
+        S           = Variable("S", 220,"ft**2", "wing planform area")
+        CL_max      = Variable("C_{L_{max, land}}", 3.5,"-", "Landing CL_max")
+        Pshaftmax   = Variable("P_{shaft-max}", 350., "hp", "Maximum shaft power")
+        return []       
+
+class simpleAircraft(Model):
+    " thing that we want to build "
+    def setup(self):
+        W           = Variable("W", "lbf", "aircraft weight")
+        Wpay        = Variable("W_{pay}", 500, "lbf", "payload weight")
+        hbatt       = Variable("h_{batt}", 210, "W*hr/kg", "battery specific energy")
+        etae        = Variable("\\eta_{e}", 0.9, "-", "total electrical efficiency")
+        Wbatt       = Variable("W_{batt}", "lbf", "battery weight")
+        AR          = Variable("AR", 10, "-", "wing aspect ratio")
+        S           = Variable("S", "ft**2", "wing planform area")
+        WS          = Variable("(W/S)", 2.5, "lbf/ft**2",
+                      "wing weight scaling factor")
+        Wwing       = Variable("W_{wing}", "lbf", "wing weight")
+        Pshaftmax   = Variable("P_{shaft-max}", "W", "max shaft power")
+        #PW = Variable("(P/W)", 0.2, "hp/lbf", "power to weight ratio")
+        sp_motor 	= Variable("sp_motor", 7./9.81, "kW/N", 'Motor specific power')
+        Wmotor      = Variable("W_{motor}", "lbf", "motor weight")
+        fstruct     = Variable("f_{struct}", 0.5, "-",
+                           "structural weight fraction")
+        Wstruct     = Variable("W_{struct}", "lbf", "structural weight")
+        b 			= Variable("b", "ft", "Wing span")
+        constraints = [W >= Wbatt + Wpay + Wwing + Wmotor + Wstruct,
+                       Wstruct >= fstruct*W,
+                       Wwing >= WS*S,
+                       Wmotor >= Pshaftmax/sp_motor,
+                       AR == b**2/S,
+                       ]
+
+        return constraints
+
+    def flight_model(self):
+        " what happens during flight "
+        return AircraftPerf(self)
+class AircraftPerf(Model):
+    " simple drag model "
+    def setup(self, aircraft):
+
+        CL = Variable("C_L", "-", "lift coefficient")
+        CD = Variable("C_D", "-", "drag coefficient")
+        cda = Variable("CDA", 0.024, "-", "non-lifting drag coefficient")
+        e = Variable("e", 0.8, "-", "span efficiency")
+
+        constraints = [CD >= cda + CL**2/pi/e/aircraft["AR"]]
+
+        return constraints
